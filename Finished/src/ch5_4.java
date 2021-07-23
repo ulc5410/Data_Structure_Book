1b0acd8d0998639f646190c27d24e8c99e23f8f6
Charles Chou <ulc5410@gmail.com>
2021-07-23 16:13:28 -0400

    structure update

--- Finished/src/ch5_4.java
+++ Finished/src/ch5_4.java
@@ -1,42 +1,41 @@
 //   程式ch5_4.java    鏈結堆疊
 import java.util.*;
+class Stack{
+    public SNODE top = null;
 
-class SNODE{ //定義節點
-    int data;
-    SNODE link;
-
-    static SNODE newsnode(int key){
-        SNODE p =new SNODE();
-        p.data = key;
-        p.link = null;
-        return p;
+    static class SNODE { //定義節點
+        int data;
+        SNODE link;
+        SNODE(int key) {
+            data = key;
+            link = null;
+        }
     }
-
-
     //插入節點p至堆疊頂端
-    static void push(SNODE stack, int key) {
-        SNODE p = newsnode(key);
-        p.link = stack.link;
-        stack.link = p;
+    void push(int key) {
+        SNODE p = new SNODE(key);
+        p.link = top;
+        top = p;
     }
 
     //取出堆疊頂端後，刪除節點
-    static int pop(SNODE stack) {
+    int pop() {
         int key;
-        key = stack.link.data;
-        stack.link = stack.link.link;
+        key=top.data;
+        top=top.link;
         return key;
     }
+
 }
 
 public class Main {
 
     public static void main(String [] args){
+        Stack s = new Stack();
         int key;
         String input;
         boolean loop = true;
         Scanner in = new Scanner(System.in); // 讀取輸入用的物件
-        SNODE top = SNODE.newsnode(-1); // -1 的下一個node為實際的top
 
         System.out.printf("***鏈結堆疊***\n");
         System.out.printf("指令如下：push,pop,exit\n");
@@ -49,13 +48,13 @@ public class Main {
                 System.out.printf("輸入資料：");
                 key = in.nextInt();
                 in.nextLine(); //去除多餘的換行
-                SNODE.push(top, key); 	   //將新資料加入堆疊
+                s.push(key); 	   //將新資料加入堆疊
             }
             else if(input.equals("pop")){
-                if(top.link == null )
+                if(s.top == null )
                     System.out.printf("空堆疊無法刪除\n");
                 else
-                    System.out.printf("%d\n",SNODE.pop(top));	 //刪除頂端資料
+                    System.out.printf("%d\n",s.pop());	 //刪除頂端資料
             }
             else if(input.equals("exit"))
                 loop = false;
--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,65 +1,65 @@
 //   程式ch5_4.java    鏈結堆疊
 import java.util.*;
-class Queue{
-    public QNODE front = null, rear = null;
+class Stack{
+    public SNODE top = null;
 
-    class QNODE{ //定義節點
+    static class SNODE { //定義節點
         int data;
-        QNODE link;
-
-        QNODE(int key) {
-            this.data = key;
-            this.link = null;
+        SNODE link;
+        SNODE(int key) {
+            data = key;
+            link = null;
         }
     }
-
-    void add(int key){       //加入資料至佇列尾端
-        QNODE p = new QNODE(key);
-        if(front == null)
-            front = p;
-        else
-            rear.link = p;
-        rear = p;
+    //插入節點p至堆疊頂端
+    void push(int key) {
+        SNODE p = new SNODE(key);
+        p.link = top;
+        top = p;
     }
 
-    int delete(){           //刪除佇列前端資料
-        int key = front.data;
-        front = front.link;
+    //取出堆疊頂端後，刪除節點
+    int pop() {
+        int key;
+        key=top.data;
+        top=top.link;
         return key;
     }
-}
 
+}
 
 public class Main {
 
     public static void main(String [] args){
-        Queue q = new Queue();
+        Stack s = new Stack();
         int key;
         String input;
         boolean loop = true;
         Scanner in = new Scanner(System.in); // 讀取輸入用的物件
 
-        System.out.printf("*** 鏈結佇列 ***\n");
-        System.out.printf("指令如下：add,delete,exit\n\n");
+        System.out.printf("***鏈結堆疊***\n");
+        System.out.printf("指令如下：push,pop,exit\n");
+
+
         do{
-            System.out.printf ("==>");
+            System.out.printf("==>");
             input = in.nextLine();
-            if(input.equals("add")){
+            if(input.equals("push")){
                 System.out.printf("輸入資料：");
                 key = in.nextInt();
                 in.nextLine(); //去除多餘的換行
-                q.add(key);	//將新資料加入佇列
+                s.push(key); 	   //將新資料加入堆疊
             }
-            else if(input.equals("delete")){
-                if(q.front == null)
-                    System.out.printf("空佇列無法刪除\n");
+            else if(input.equals("pop")){
+                if(s.top == null )
+                    System.out.printf("空堆疊無法刪除\n");
                 else
-                    System.out.printf("%d\n",q.delete());  //刪除佇列前端節點
+                    System.out.printf("%d\n",s.pop());	 //刪除頂端資料
             }
             else if(input.equals("exit"))
                 loop = false;
             else
                 System.out.printf("輸入錯誤\n");
-        }while(loop);
+        } while(loop);
     }
 }
